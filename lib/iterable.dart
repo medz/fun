fc02f1f0b59424df3d9ee79ef4fa36778eb02313
Seven Du <whine.whitish.0u@icloud.com>
2024-04-12 02:35:53 +0800

    feat(iterable): cluster

--- lib/iterable.dart
+++ lib/iterable.dart
@@ -20,4 +20,37 @@ extension FunIterable<T> on Iterable<T> {
             : getter(a).compareTo(getter(b)),
       );
   }
+
+  /// Split a list into many lists of the given size.
+  ///
+  /// Given an array of items and a desired cluster size (`n`), returns an
+  /// array of arrays.
+  /// Each child array containing `n` (cluster size) items split as evenly as
+  /// possible.
+  ///
+  /// Example:
+  ///
+  /// ```dart
+  /// final data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  /// final clusters = data.cluster(3);
+  ///
+  /// print(clusters); // [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  /// ```
+  ///
+  /// - `size`: The size of each cluster.
+  Iterable<Iterable<T>> cluster(int size) sync* {
+    for (int i = 0; i < length; i += size) {
+      yield skip(i).take(size);
+    }
+  }
+}
+
+void main(List<String> args) {
+  // final list = ['c', 'a', 'b'];
+  // print(list.alphabetical((item) => item)); // ['a', 'b', 'c']
+  // print(list.alphabetical((item) => item, desc: true)); // ['c', 'b', 'a']
+
+  final data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  final clusters = data.cluster(3);
+  print(clusters); // [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 }
--- test/iterable_test.dart
+++ test/iterable_test.dart
@@ -8,4 +8,15 @@ void main() {
     expect(
         list.alphabetical((item) => item, desc: true), equals(['c', 'b', 'a']));
   });
+
+  test('cluster', () {
+    final data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    final clusters = data.cluster(3);
+    final expected = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    expect(clusters, equals(expected));
+  });
 }
