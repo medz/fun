c9dd743d9e81fa20dbe714d63b6ef5d641a6b6d9
Seven Du <whine.whitish.0u@icloud.com>
2024-04-12 02:51:33 +0800

    feat(iterable): diff

--- lib/iterable.dart
+++ lib/iterable.dart
@@ -1,7 +1,7 @@
 library iterable;
 
 extension FunIterable<T> on Iterable<T> {
-  /// Sorts an array of objects alphabetically by a property.
+  /// Sorts an [Iteravle] of [T] alphabetically by a property.
   ///
   /// Example:
   /// ```dart
@@ -23,8 +23,8 @@ extension FunIterable<T> on Iterable<T> {
 
   /// Split a list into many lists of the given size.
   ///
-  /// Given an array of items and a desired cluster size (`n`), returns an
-  /// array of arrays.
+  /// Given an [Iteravle] of items and a desired cluster size (`n`), returns an
+  /// [Iteravle] of [Iteravle]s.
   /// Each child array containing `n` (cluster size) items split as evenly as
   /// possible.
   ///
@@ -44,12 +44,7 @@ extension FunIterable<T> on Iterable<T> {
     }
   }
 
-  /// Creates an object with counts of occurrences of items in an array.
-  ///
-  /// Given an array of objects and an identity callback function to determine
-  /// how each object should be identified. Returns an object where the keys
-  /// are the id values the callback returned and each value is an integer
-  /// telling how many times that id occurred.
+  /// Creates an object with counts of occurrences of items in an [Iterable].
   ///
   /// Example:
   ///
@@ -70,16 +65,31 @@ extension FunIterable<T> on Iterable<T> {
   /// print(counts2); // {a: 2, b: 2, c: 1}
   /// ```
   ///
-  /// - `identity`: The callback function to determine the identity of each
+  /// - `getter`: The callback function to determine the identity of each
   /// item.
-  Map<K, int> counting<K>(K Function(T item) identity) {
+  Map<K, int> counting<K>(K Function(T item) getter) {
     final result = <K, int>{};
     for (final item in this) {
-      final key = identity(item);
+      final key = getter(item);
       result[key] = (result[key] ?? 0) + 1;
     }
 
     return result;
+  }
+
+  /// Create an [Iterable] of differences between two [Iteraabl]s.
+  ///
+  /// Example:
+  /// ```dart
+  /// final list1 = [1, 2, 3, 4, 5];
+  /// final list2 = [1, 2, 3, 4, 6];
+  /// final diff = list1.difference(list2);
+  /// print(diff); // [5]
+  /// ```
+  ///
+  /// - `other`: The other [Iterable] to compare against.
+  Iterable<T> diff(Iterable<T> other) {
+    return where((item) => !other.contains(item));
   }
 }
 
@@ -92,17 +102,22 @@ void main(List<String> args) {
   // final clusters = data.cluster(3);
   // print(clusters); // [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-  final list = ['a', 'b', 'a', 'c', 'b', 'a'];
-  final counts = list.counting((item) => item);
-  print(counts); // {a: 3, b: 2, c: 1}
+  // final list = ['a', 'b', 'a', 'c', 'b', 'a'];
+  // final counts = list.counting((item) => item);
+  // print(counts); // {a: 3, b: 2, c: 1}
 
-  final list2 = [
-    (1, 'a'),
-    (2, 'b'),
-    (3, 'a'),
-    (4, 'c'),
-    (5, 'b'),
-  ];
-  final counts2 = list2.counting((item) => item.$2);
-  print(counts2); // {a: 2, b: 2, c: 1}
+  // final list2 = [
+  //   (1, 'a'),
+  //   (2, 'b'),
+  //   (3, 'a'),
+  //   (4, 'c'),
+  //   (5, 'b'),
+  // ];
+  // final counts2 = list2.counting((item) => item.$2);
+  // print(counts2); // {a: 2, b: 2, c: 1}
+
+  final list1 = [1, 2, 3, 4, 5];
+  final list2 = [1, 2, 3, 4, 6];
+  final diff = list1.diff(list2);
+  print(diff); // [5]
 }
